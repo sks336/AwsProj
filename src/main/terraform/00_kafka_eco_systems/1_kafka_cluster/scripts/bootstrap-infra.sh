c2a54ba571e65473ddb7c260113cc94840631db5
sks336@gmail.com <sks336@gmail.com>
2024-07-15 13:32:07 +0800

    Implementing Prometheus Monitoring

--- src/main/terraform/00_kafka_eco_systems/1_kafka_cluster/scripts/bootstrap-infra.sh
+++ src/main/terraform/00_kafka_eco_systems/1_kafka_cluster/scripts/bootstrap-infra.sh
@@ -6,6 +6,19 @@ PROJ_DIR=$SCRIPT_DIR/..
 
 cd $PROJ_DIR
 
+downloadLib_Locally() {
+
+  LIB_DIR=${SCRIPT_DIR}/../resources_00_tmp/lib
+
+  if [[ ! -f ${LIB_DIR}/prometheus-2.22.0.linux-amd64.tar.gz ]]; then
+    echo 'Prometheus jar does not exists, downloading....'
+    wget https://github.com/prometheus/prometheus/releases/download/v2.22.0/prometheus-2.22.0.linux-amd64.tar.gz --no-check-certificate -O ${LIB_DIR}/prometheus-2.22.0.linux-amd64.tar.gz
+  fi
+
+}
+
+downloadLib_Locally
+
 terraform init -input=false
 
 terraform apply -input=false -auto-approve
