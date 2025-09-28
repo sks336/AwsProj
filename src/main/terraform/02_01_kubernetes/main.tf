80d9de3d651c33806d3119e52f9df6711bd9c2c8
sks336@gmail.com <sks336@gmail.com>
2025-09-29 01:45:25 +0800

    commit

--- src/main/terraform/02_01_kubernetes/main.tf
+++ src/main/terraform/02_01_kubernetes/main.tf
@@ -49,7 +49,6 @@ resource "aws_instance" "kube_node_control_plane" {
       type    = "ssh"
       port    = 22
       user    = "ubuntu"
-      key_name      = var.pem_key_name
       private_key = file("${var.pem_file}")
       timeout = "2m"
       agent   = false
@@ -80,7 +79,6 @@ resource "aws_instance" "kube_node_masters" {
       type    = "ssh"
       port    = 22
       user    = "ubuntu"
-      key_name      = var.pem_key_name
       private_key = file("${var.pem_file}")
       timeout = "2m"
       agent   = false
@@ -116,7 +114,6 @@ resource "aws_instance" "kube_node_workers" {
       type    = "ssh"
       port    = 22
       user    = "ubuntu"
-      key_name      = var.pem_key_name
       private_key = file("${var.pem_file}")
       timeout = "2m"
       agent   = false
