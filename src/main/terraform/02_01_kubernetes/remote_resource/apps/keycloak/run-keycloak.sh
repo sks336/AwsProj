7142b913745f5c8abfbbfeea3c23df87a0e08e7f
sks336@gmail.com <sks336@gmail.com>
2025-10-05 18:56:56 +0800

    keycloak readme

--- src/main/terraform/02_01_kubernetes/remote_resource/apps/keycloak/run-keycloak.sh
+++ src/main/terraform/02_01_kubernetes/remote_resource/apps/keycloak/run-keycloak.sh
@@ -5,11 +5,16 @@ echo "SCRIPTS_DIR: $SCRIPTS_DIR"
 
 NS=ns-keycloak
 
+kubectl delete ns $NS
+
+sleep 3
+
 kubectl get namespace $NS >/dev/null 2>&1 || kubectl create namespace $NS
 
 
 kubectl -n $NS apply -f $SCRIPTS_DIR/kc.yaml
 
+sleep 3
 
 echo "Access the Keycloak at http://kube.techlearning.me:32100"
 echo "Username: admin"
