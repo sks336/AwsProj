07b8d1ae96931732e570f1f444b8683979b420c9
$achin <sks336@gmail.com>
2025-10-06 14:20:59 +0800

    Update run-redis.sh
    Patching redis service for NodePort

--- src/main/terraform/02_01_kubernetes/remote_resource/apps/redis/run-redis.sh
+++ src/main/terraform/02_01_kubernetes/remote_resource/apps/redis/run-redis.sh
@@ -4,6 +4,7 @@ SCRIPTS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 echo "SCRIPTS_DIR: $SCRIPTS_DIR"
 
 NS=ns-redis
+helm -n $NS delete redis || true;
 
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
@@ -13,7 +14,17 @@ helm install -n $NS redis bitnami/redis \
   --create-namespace \
   --set global.security.allowInsecureImages=true
 
+sleep 3
 
+kubectl patch svc redis -n $NS -p '{
+  "spec": {
+    "type": "NodePort",
+    "ports": [
+      {"port": 6379, "nodePort": 32000, "protocol": "TCP", "name": "redis"},
+      {"port": 26379, "nodePort": 32001, "protocol": "TCP", "name": "sentinel"}
+    ]
+  }
+}'
 
 
 
