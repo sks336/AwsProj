8a2be23369acbca7fe4ec6cf59dcb8e353074eac
sks336@gmail.com <sks336@gmail.com>
2025-10-09 00:04:04 +0800

    Harbor

---
+++ src/main/terraform/02_01_kubernetes/remote_resource/apps/harbor/run-harbor.sh
@@ -0,0 +1,29 @@
+#!/bin/env bash
+
+SCRIPTS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+echo "SCRIPTS_DIR: $SCRIPTS_DIR"
+
+NS=ns-harbor
+APP_NAME=harbor
+
+helm -n $NS delete $APP_NAME || true;
+kubectl delete ns $NS || true;
+
+
+helm repo add harbor https://helm.goharbor.io
+helm repo update
+
+
+
+
+helm -n $NS install $APP_NAME harbor/harbor \
+  -f $SCRIPTS_DIR/values.yaml \
+  --create-namespace
+
+
+echo "Access Harbor UI at : http://kube.techlearning.me:30003/harbor/projects"
+echo "Username: admin"
+echo "Password: admin123"
+
+
+
