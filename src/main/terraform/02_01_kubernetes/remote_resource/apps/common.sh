b745482f68df82f757e888205660d50c710e8991
sks336@gmail.com <sks336@gmail.com>
2025-10-10 04:10:25 +0800

    commit

--- src/main/terraform/02_01_kubernetes/remote_resource/apps/common.sh
+++ src/main/terraform/02_01_kubernetes/remote_resource/apps/common.sh
@@ -1,6 +1,16 @@
 #!/bin/env bash
 
+SCRIPTS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+echo "inside common.sh file....SCRIPTS_DIR: $SCRIPTS_DIR"
+
+
 echo "Going to create Local Path Provisoner...."
 kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/master/deploy/local-path-storage.yaml
 kubectl patch storageclass local-path -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
 echo "Local Path Provisoner created...."
+
+
+echo "Deploying Nginx Ingress Controller!!!"
+#kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/cloud/deploy.yaml
+
+kubectl apply -f $SCRIPTS_DIR/nginx_ingress_controller.yaml
