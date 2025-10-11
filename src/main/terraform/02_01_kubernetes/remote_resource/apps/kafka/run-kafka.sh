2441c74e50ae9e31e046b8a480214b77e1fd3591
sks336@gmail.com <sks336@gmail.com>
2025-10-12 03:45:59 +0800

    Adding kafka and zk

--- src/main/terraform/02_01_kubernetes/remote_resource/apps/kafka/run-kafka.sh
+++ src/main/terraform/02_01_kubernetes/remote_resource/apps/kafka/run-kafka.sh
@@ -4,23 +4,15 @@ SCRIPTS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 echo "SCRIPTS_DIR: $SCRIPTS_DIR"
 
 NS=ns-kafka
-APP_NAME=kafka
 
-helm -n $NS delete $APP_NAME || true;
 kubectl delete ns $NS || true;
-
-
 kubectl get namespace $NS >/dev/null 2>&1 || kubectl create namespace $NS
 
-helm repo add bitnami https://charts.bitnami.com/bitnami
-helm repo update
-
-
-helm -n $NS install kafka bitnami/kafka -f $SCRIPTS_DIR/values.yaml
-
-sleep 3
-
-kubectl -n $NS apply -f $SCRIPTS_DIR/ingress-${APP_NAME}.yaml
+kubectl apply -f $SCRIPTS_DIR/zk.yaml
+sleep 1
+kubectl apply -f $SCRIPTS_DIR/kafka.yaml
+#sleep 1
+#kubectl -n $NS apply -f $SCRIPTS_DIR/ingress-${APP_NAME}.yaml
 
 
 
